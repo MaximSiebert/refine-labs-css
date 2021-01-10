40b44337ba12eb12765e2e0d97b4eae8f75e6300
Maxim Siebert <maximsiebert@maxims-mbp.lan>
2021-01-10 15:31:35 -0500

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
-const { fontFamily } = require('tailwindcss/defaultTheme');
+const { colors, fontFamily } = require('tailwindcss/defaultTheme');
 module.exports = {
     purge: [],
     theme: {
       extend: {
+        colors: {
+          green: {
+            ...colors.green,
+            '400': '#23dc70',
+          },
+          blue: {
+            ...colors.blue,
+            '400': '#3eacf8',
+          },
+          white: 'e1e1f6',
+          gray: {
+            ...colors.gray,
+            '400': '80a0b7',
+            '700': '424155',
+            '900': '151517',
+          }
+        }
       },
       fontFamily: {
         sans: ['"HKSentiments"', ...fontFamily.sans],
