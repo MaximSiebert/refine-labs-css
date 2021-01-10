3639912858f1b9884d3802ac0087acdd853fdfeb
Maxim Siebert <maximsiebert@maxims-mbp.lan>
2021-01-10 15:46:18 -0500

    transition

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,12 +21,13 @@ module.exports = {
           }
         },
         boxShadow: {
-          sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
-          DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
-          md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
-          lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(35, 219, 111, 0.33)',
-          xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(35, 219, 111, 0.33)',
-          inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+          sm: '0 1px 2px 0 rgba(35, 219, 111, 0.1)',
+          DEFAULT: '0 1px 3px 0 rgba(35, 219, 111, 0.33), 0 1px 2px 0 rgba(35, 219, 111, 0.1)',
+          md: '0 4px 6px -1px rgba(35, 219, 111, 0.33), 0 2px 4px -1px rgba(35, 219, 111, 0.1)',
+          lg: '0 10px 15px -3px rgba(35, 219, 111, 0.33), 0 4px 6px -2px rgba(35, 219, 111, 0.1)',
+          xl: '0 20px 25px -5px rgba(35, 219, 111, 0.33), 0 10px 10px -5px rgba(35, 219, 111, 0.1)',
+          '2xl': '0 25px 50px -12px rgba(35, 219, 111, .4)',
+          inner: 'inset 0 2px 4px 0 rgba(35, 219, 111, 0.2)',
           none: 'none',
         }
       },
